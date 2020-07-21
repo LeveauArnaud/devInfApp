bb54bb99dd7097245ea4db97d7e8912d26f2fd87
LeveauArnaud <a.leveau@students.ephec.be>
2020-07-21 15:29:26 +0200

    dev de la view

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 import controller.Controller;
 import model.Model;
-import view.View;
+import view.MainView;
 
 public class App {
 	
  public static void main(String[] args) {
   // Assemble all the pieces of the MVC
   Model m = new Model("Sylvain", "Saurel");
-  View v = new View("Scout App");
+  MainView v = new MainView();
   Controller c = new Controller(m, v);
   c.initController();
  }
